0a43620325a0652f4b025f311d0b1097d1a1f2db
Dar Levy <darlevy8@gmail.com>
2024-06-05 12:34:47 +0300

    Implement for Int Char and Bool

--- HW4.hs
+++ HW4.hs
@@ -9,74 +9,130 @@
 
 module HW4 where
 
-import Data.Char (chr, ord)
+import Data.Char (ord, chr)
 import Data.Either
-import Data.List
-import Data.Maybe
+import Data.List (unfoldr)
+import Data.Maybe (fromJust, isJust)
 import Data.Semigroup (Arg (..))
 import EqMap (EqMap)
-import EqMap qualified
+import EqMap qualified as EM
 import EqSet (EqSet)
-import EqSet qualified
+import EqSet qualified as ES
 
--- Section 2: Serialization
 class Serializable a where
   serialize :: a -> [Int]
   deserialize :: [Int] -> a
 
-instance Serializable Int
-instance Serializable Bool
-instance Serializable Char
-instance Serializable a => Serializable (Maybe a)
-instance (Serializable a, Serializable b) => Serializable (a, b)
-instance (Serializable a, Serializable b) => Serializable (Either a b)
-instance Serializable a => Serializable [a]
-instance (Serializable a, Eq a) => Serializable (EqSet a)
-instance (Serializable k, Eq k, Serializable v) => Serializable (EqMap k v)
+instance Serializable Int where
+  serialize x = [x]
+  deserialize [x] = x
+  deserialize _ = error "Invalid input for Int deserialization"
 
--- Section 3: Metric
-infinity :: Double
-infinity = 1 / 0
+instance Serializable Bool where
+  serialize True = [1]
+  serialize False = [0]
+  deserialize [1] = True
+  deserialize [0] = False
+  deserialize _ = error "Invalid input for Bool deserialization"
 
-class Eq a => Metric a where
-  distance :: a -> a -> Double
+instance Serializable Char where
+  serialize x = [ord x]
+  deserialize [x] = chr x
+  deserialize _ = error "Invalid input for Char deserialization"
 
-instance Metric Double
-instance Metric Int
-instance Metric Char
+-- Instance for Maybe a
+--instance Serializable a => Serializable (Maybe a) where
+--  serialize Nothing = [-1]
+--  serialize (Just x) = 0 : serialize x
+--  deserialize (-1:_) = Nothing
+--  deserialize (0:xs) = Just (deserialize xs)
+--  deserialize _ = error "Invalid input for Maybe deserialization"
+--
+---- Instance for (a, b)
+--instance (Serializable a, Serializable b) => Serializable (a, b) where
+--  serialize (x, y) = serialize x ++ serialize y
+--  deserialize xs =
+--    let (xRest, xs') = splitAt (length (serialize (undefined :: a))) xs
+--        (yRest, ys') = splitAt (length (serialize (undefined :: b))) xs'
+--    in (deserialize xRest, deserialize yRest)
+--
+---- Instance for Either a b
+--instance (Serializable a, Serializable b) => Serializable (Either a b) where
+--  serialize (Left x) = 0 : serialize x
+--  serialize (Right y) = 1 : serialize y
+--  deserialize (0:xs) = Left (deserialize xs)
+--  deserialize (1:xs) = Right (deserialize xs)
+--  deserialize _ = error "Invalid input for Either deserialization"
+--
+---- Instance for [a]
+--instance Serializable a => Serializable [a] where
+--  serialize [] = [-1]
+--  serialize (x:xs) = 0 : serialize x ++ serialize xs
+--  deserialize (-1:_) = []
+--  deserialize (0:xs) = deserialize' xs
+--    where
+--      deserialize' [] = []
+--      deserialize' ys = let (xRest, ys') = splitAt (length (serialize (undefined :: a))) ys
+--                        in deserialize xRest : deserialize ys'
+--
+---- Rename newtype wrappers to avoid ambiguity
+--newtype SerializableEqSet a = SerializableEqSet (ES.EqSet a) deriving (Show, Eq)
+--
+--instance (Serializable a, Ord a) => Serializable (SerializableEqSet a) where
+--  serialize (SerializableEqSet s) = serialize (ES.elems s)
+--  deserialize xs = let deserializedList = deserialize xs :: [a]
+--                   in SerializableEqSet (foldr ES.insert ES.empty deserializedList)
+--
+--newtype SerializableEqMap k v = SerializableEqMap (EM.EqMap k v) deriving (Show, Eq)
+--
+--instance (Serializable k, Ord k, Serializable v) => Serializable (SerializableEqMap k v) where
+--  serialize (SerializableEqMap m) = serialize (EM.assocs m)
+--  deserialize xs = let deserializedList = deserialize xs :: [(k, v)]
+--                   in SerializableEqMap (foldr (uncurry EM.insert) EM.empty deserializedList)
 
--- Euclidean distance
-instance (Metric a, Metric b) => Metric (a, b)
-
-data ManhattanTuple a b = ManhattanTuple a b deriving Eq
-instance (Metric a, Metric b) => Metric (ManhattanTuple a b)
-
--- Just and Nothing have distance of infinity.
--- Two Justs measure the distance between the two values.
-instance Metric a => Metric (Maybe a)
-
--- Left and Right have a distance of infinity.
--- Same constructores measure the distance between the two values.
-instance (Metric a, Metric b) => Metric (Either a b)
-
--- Lists of different sizes have distance of infinity.
--- Euclidean distance.
-instance Metric a => Metric [a]
-
-newtype ManhattanList a = ManhattanList [a] deriving Eq
-instance Metric a => Metric (ManhattanList a)
-
--- Returns the element with the shortest distance to the input.
--- If there are no numbers whose distance is less than infinity, return Nothing.
-closest :: Metric a => a -> [a] -> Maybe a
--- Similar to the above, but uses a function move the element
--- to another metric space.
-closestOn :: Metric b => (a -> b) -> a -> [a] -> Maybe a
--- Will not swap elements whose distance is less than d, even if their
--- order implies they should be swapped.
-metricBubbleSort :: (Metric a, Ord a) => Double -> [a] -> [a]
--- Similar to the above, but uses a function to extract the value used for sorting.
-metricBubbleSortOn :: (Metric b, Ord b) => (a -> b) -> Double -> [a] -> [a]
-
--- Bonus (10 points).
-clusters :: Metric a => [a] -> [[a]]
+---- Section 3: Metric
+--infinity :: Double
+--infinity = 1 / 0
+--
+--class Eq a => Metric a where
+--  distance :: a -> a -> Double
+--
+--instance Metric Double
+--instance Metric Int
+--instance Metric Char
+--
+---- Euclidean distance
+--instance (Metric a, Metric b) => Metric (a, b)
+--
+--data ManhattanTuple a b = ManhattanTuple a b deriving Eq
+--instance (Metric a, Metric b) => Metric (ManhattanTuple a b)
+--
+---- Just and Nothing have distance of infinity.
+---- Two Justs measure the distance between the two values.
+--instance Metric a => Metric (Maybe a)
+--
+---- Left and Right have a distance of infinity.
+---- Same constructores measure the distance between the two values.
+--instance (Metric a, Metric b) => Metric (Either a b)
+--
+---- Lists of different sizes have distance of infinity.
+---- Euclidean distance.
+--instance Metric a => Metric [a]
+--
+--newtype ManhattanList a = ManhattanList [a] deriving Eq
+--instance Metric a => Metric (ManhattanList a)
+--
+---- Returns the element with the shortest distance to the input.
+---- If there are no numbers whose distance is less than infinity, return Nothing.
+--closest :: Metric a => a -> [a] -> Maybe a
+---- Similar to the above, but uses a function move the element
+---- to another metric space.
+--closestOn :: Metric b => (a -> b) -> a -> [a] -> Maybe a
+---- Will not swap elements whose distance is less than d, even if their
+---- order implies they should be swapped.
+--metricBubbleSort :: (Metric a, Ord a) => Double -> [a] -> [a]
+---- Similar to the above, but uses a function to extract the value used for sorting.
+--metricBubbleSortOn :: (Metric b, Ord b) => (a -> b) -> Double -> [a] -> [a]
+--
+---- Bonus (10 points).
+--clusters :: Metric a => [a] -> [[a]]
